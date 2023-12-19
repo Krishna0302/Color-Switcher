0301b7185697a26e590b511bcae36c3d6800f6bf
Krishna0302 <133387126+Krishna0302@users.noreply.github.com>
2023-12-19 05:03:23 +0000

    js

---
+++ 03_script.js
@@ -0,0 +1,19 @@
+const buttons = document.querySelectorAll(.button)
+const body = document.querySelector("body")
+
+buttons.forEach(function (button) {
+    button.addEventListener('click', function(e){
+        if(e.target.id==="gray"){
+            body.backgroundColor = e.target.id;
+        }
+        if(e.target.id==="white"){
+            body.backgroundColor = e.target.id;
+        }
+        if(e.target.id==="blue"){
+            body.backgroundColor = e.target.id;
+        }
+        if(e.target.id==="yellow"){
+            body.backgroundColor = e.target.id;
+        }
+    });
+});
